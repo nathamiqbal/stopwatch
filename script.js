22de8e7b61d280432c024dd4ac758a0aa6af9052
nathamiqbal <83658878+nathamiqbal@users.noreply.github.com>
2021-08-22 11:07:23 +0530

    added  millisecond function

--- script.js
+++ script.js
@@ -14,8 +14,9 @@ function timeToString(time) {
   let formattedHH = hh.toString().padStart(2, "0");
   let formattedMM = mm.toString().padStart(2, "0");
   let formattedSS = ss.toString().padStart(2, "0");
+  let formattedMS = ms.toString().padStart(2, "0");
   
-  return `${formattedHH}:${formattedMM}:${formattedSS}`;
+  return `${formattedHH}:${formattedMM}:${formattedSS}:${formattedMS}`;
 
 }
 //variables declaration
